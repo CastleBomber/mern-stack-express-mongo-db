97498cf8f4723efaf439dbb5ff2e3c0f3daa15e0
cBombs <richie.ucsb@gmail.com>
2023-01-24 20:19:49 -0800

    npm run dev does both backend and frontend of client, server

--- mern-tutorial/frontend/src/App.js
+++ mern-tutorial/frontend/src/App.js
@@ -1,3 +1,16 @@
+/**
+ *  Author: CastleBomber
+ *  Project: Mern-Tutorial
+ *  Date: January 14th, 2023
+ *
+ *  Acknowledgements: Traversy Media's Youtube MERN stack series
+ *
+ *  Tips:
+ *  npm run server
+ *  npm run client
+ *  npm run dev - runs both server && client
+ *  cmd+D will change opening and closing tag simultaneously
+ */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
